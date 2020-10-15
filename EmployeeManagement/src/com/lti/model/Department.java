a7d554ea55fae683f3d0abc1481b6db27314f205
HP <HP@DESKTOP-DEH4JOP>
2020-10-15 13:19:35 +0530

    Department class added

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,20 @@
+package com.lti.model;
+
+public class Department {
+	int depno;
+	String depName;
+	public int getDepno() {
+		return depno;
+	}
+	public void setDepno(int depno) {
+		this.depno = depno;
+	}
+	public String getDepName() {
+		return depName;
+	}
+	public void setDepName(String depName) {
+		this.depName = depName;
+	}
+	
+
+}
